0fc1be3f4bc151e590be7d27f17e150f279a3f34
Caio Carvalho <21188280+lowercase00@users.noreply.github.com>
2022-08-09 13:41:32 -0300

    Runner Results doc strings

--- suitable/runner_results.py
+++ suitable/runner_results.py
@@ -1,32 +1,69 @@
+import typing as t
+
+
 class RunnerResults(dict):
-    """ Wraps the results of parsed module_runner output. The result may
-    be used just like it is in Ansible:
+    """
+    Wraps the results of parsed module_runner output. The result may
+    be used just like it is in Ansible::
+        
+        >>> result['contacted']['server']['rc']
 
-    result['contacted']['server']['rc']
+    Alternatively can be used accessing keys as methods::
 
-    or it can alternatively be used thusly:
-
-    result.rc('server')
+        >>> result.contacted()
+        >>> result.rc('server')
 
     """
 
-    def __init__(self, results):
+    def __init__(self, results: dict[t.Any, t.Any]):
+        """
+        Initializes the class with a dictionary of results.
+
+        Args:
+            results (dict[t.Any, t.Any]): A dicionary.
+        """        
         self.update(results)
 
-    def __getattr__(self, key):
+    def __getattr__(self, key: str):
+        """
+        Dunder method to make a dict behaviour as keys were methods.
+        Allows for::
+
+            >>> result['contacted']
+            >>> result.contacted()
+
+        Args:
+            key (str): The key to get
+
+        Returns:
+            t.Any: ...
+        """        
         return lambda server=None: self.acquire(server, key)
 
-    def acquire(self, server, key):
+    def acquire(self, server: t.Optional[str] = None, key: t.Optional[str] = None):
+        """
+        Aquires the result for a given server and key. If no server is given,
+        and exactly one contacted server exists return the value of said server directly.
 
-        # if no server is given and exactly one contacted server exists
-        # return the value of said server directly
-        if server is None and len(self['contacted']) == 1:
-            server = next((k for k in self['contacted'].keys()), None)
+        Args:
+            server (t.Optional[str], optional): The server to acquire the value. Defaults to None.
+            key (t.Optional[str], optional): The key to get. Defaults to None.
 
-        if server not in self['contacted']:
+        Raises:
+            KeyError: If no servers are contacted or if the server is not found.
+            AttributeError: If key was not found
+
+        Returns:
+            t.Any: ...
+        """
+        contacted_servers: t.Optional[dict] = self['contacted']
+        if server is None and len(contacted_servers) == 1:
+            server = next((k for k in contacted_servers.keys()), None)
+
+        if server not in contacted_servers:
             raise KeyError("{} could not be contacted".format(server))
 
-        if key not in self['contacted'].get(server, {}):
+        if key not in contacted_servers.get(server, {}):
             raise AttributeError
 
-        return self['contacted'][server][key]
+        return contacted_servers[server][key]
